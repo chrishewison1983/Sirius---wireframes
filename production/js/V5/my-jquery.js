e6afca3f2bb56dd638802e2db4e859eb064e080a
chrishewison1983 <ods.chrishewison@gmail.com>
2017-02-22 13:29:56 +0000

    Publish files

---
+++ production/js/V5/my-jquery.js
@@ -0,0 +1,26 @@
+// Checkbox code
+$('input:checkbox').change(function(){
+     if($(this).is(":checked")) {
+          $(this).parent().addClass("checked");
+     } else {
+          $(this).parent().removeClass("checked");
+     }
+});
+
+// Log in code
+$('input#password').click(function(){
+     $("#login-button").removeClass("de-activate");
+});
+
+// Tabs code
+$(document).ready(function(){
+     $('ul.tabs li').click(function(){
+     	var tab_id = $(this).attr('data-tab');
+
+     	$('ul.tabs li').removeClass('current');
+     	$('.tab-content').removeClass('current');
+
+     	$(this).addClass('current');
+     	$("#"+tab_id).addClass('current');
+     })
+});
