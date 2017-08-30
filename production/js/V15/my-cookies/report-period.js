d32bad985e28de5db4973956c3883c2ff1ccc0bb
chrishewison1983 <ods.chrishewison@gmail.com>
2017-08-30 13:54:02 +0100

    Changes to the tickets: 435, 627, 387, 612 & 986

--- production/js/V15/my-cookies/report-period.js
+++ production/js/V15/my-cookies/report-period.js
@@ -1,10 +1,22 @@
 // =================================== Create Task - COOKIES =================================== //
 $("#reporting-period-button").on("click", function (e) {
-
      $.cookie("reporting-period-completed", true, {path:'/'});
-
-     // Cookies for side bar
-
 });
+
+// Decides the user journey
+$(".edit-property").on("click", function (e) {
+     $.cookie("edit-property", true, {path:'/'});
+});
+
+$(".edit-health").on("click", function (e) {
+     $.cookie("edit-property", false, {path:'/'});
+});
+
+if ($.cookie("edit-property") == "true") {
+     $(".report-level").show();
+} else {
+     $(".report-level").hide();
+}
+
 
 // Populate side bar
--- production/js/V15/my-jquery/report-period.js
+++ production/js/V15/my-jquery/report-period.js
@@ -1,2 +1,33 @@
 // =================================== Reporting period - JAVASCRIPT =================================== //
 // $('.reporting-period-journey #cta-footer').hide();
+
+$('.edit-report-period').hide();
+
+$('#reporting-period-button').hide();
+
+$("#edit-reporting-period").click( function(e){
+     e.preventDefault();
+     $('.form-group').removeClass('read-only');
+     $('input[type="text"]').removeAttr('readonly');
+     $('.edit-report-period').slideDown();
+     $(this).hide();
+     $('#reporting-period-button').show();
+     $('#report-level-min, #report-level-gen').removeAttr('disabled');
+});
+
+$('#report-why').change(function(){
+     $('#reporting-period-button').removeClass('de-activate');
+     $('#reporting-period-button').attr('href', '#edit-report');
+     $('#reporting-period-button').attr('rel', 'modal:open');
+});
+
+// General report level items shown
+$('.general-report-level').hide();
+
+$('#report-level-gen').change(function(){
+     if($(this).is(":checked")) {
+          $('.general-report-level').slideDown();
+     } else {
+          $('.general-report-level').slideUp();
+     }
+});
